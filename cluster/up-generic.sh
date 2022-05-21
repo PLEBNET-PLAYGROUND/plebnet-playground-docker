8d24c9643cd07da8ab04250f5754fae0fac5bdcf
Richard Safier <richardj@safier.com>
2022-05-21 17:28:17 -0400

    make up-generic.sh autodetect TRIPLET

--- cluster/up-generic.sh
+++ cluster/up-generic.sh
@@ -2,7 +2,22 @@
 if [ -z "$1" ]; then
     $1=5
 fi
-: ${TRIPLET:=x86_64-linux-gnu}
+echo "Auto Detect"
+    if [ "$(uname -m)" == "arm64" ]; then
+        echo "ARM Chip found"
+        if [ "$(sysctl -n machdep.cpu.brand_string)" == "Apple M1" ]; then
+            echo "M1 ARM detected"
+            export TRIPLET="aarch64-linux-gnu"
+        else
+            export TRIPLET="arm64-linux-gnu"
+        fi
+    elif [ "$(uname -m)" == "x86_64" ]; then
+        #echo 'You must provide TRIPLET as first parameter'
+        #echo './install.sh x86_64-linux-gnu'
+        export TRIPLET="x86_64-linux-gnu"
+    fi
+
+# : ${TRIPLET:=x86_64-linux-gnu}
 : ${bitcoind=$1}
 : ${lnd=$1}
 : ${tor=$1}
