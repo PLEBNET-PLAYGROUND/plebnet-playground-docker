2cc36260869fcc6df4b7de8ea29a766cca7dd55b
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-06-02 11:49:47 -0400

    uninstall.sh: add uninstall cluster/volumes to dialogue

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 docker-compose down
+pushd cluster && docker-compose down && popd
 mkdir -p volumes
+mkdir -p cluster/volumes
 uninstall (){
 
 # default is to do nothing
@@ -8,15 +10,20 @@ read -p "This will destroy private keys! Continue? (y/n) " -n 1;
 echo "	";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo rm -rf volumes;
+    sudo rm -rf cluster/volumes;
 else
     read -p "Create a backup of the volumes folder? Continue? (y/n) " -n 1;
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo "	";
         TIME=$(date +%s)
         BAK=volumes-$TIME
+        CLUSTER_BAK=cluster/volumes-$TIME
         mv volumes ./$BAK
+        mv cluster/volumes $CLUSTER_BAK
         echo $BAK created...
+        echo $CLUSTER_BAK created...
         ls -ld volumes-*
+        ls -ld cluster/volumes-*
     else
         exit 0;
     fi
